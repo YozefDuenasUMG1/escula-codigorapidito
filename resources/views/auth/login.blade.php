6e2789d5415feee682eeaef2c00d3a5d1fd03d5a
YozefDuenasUMG1 <yduenass2@miumg.edu.gt>
2025-05-30 17:10:50 -0600

    secure_asset()actualizacions

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio de Sesión</title>
-    <link rel="stylesheet" href="/css/loginstyle.css">
+    <link rel="stylesheet" href="{{ secure_asset('css/loginstyle.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" rel="stylesheet">
 </head>
 <body>
